bf982f6d83b0b6f1974a0a1c373c317daf28f7d7
Guilherme Barile <guigouz@gmail.com>
2015-02-22 07:53:33 -0300

    table tests

---
+++ test/TableTest.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Tests for the DB\Table
+ */
+include dirname(__DIR__) . '/vendor/autoload.php';
+
+use Objectiveweb\DB;
+use Objectiveweb\DB\Table;
+
+class TableTest extends PHPUnit_Framework_TestCase
+{
+    /** @var  Table */
+    static protected $table;
+
+    public static function setUpBeforeClass()
+    {
+        $db = new DB('mysql:dbname=objectiveweb;host=192.168.56.101', 'root');
+        $db->query('drop table if exists db_test')->exec();
+
+        $db->query('create table db_test
+            (`id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                `name` VARCHAR(255));')->exec();
+
+        self::$table = $db->table('db_test');
+    }
+
+    public function testInsert()
+    {
+        $id = self::$table->post(['name' => 'test']);
+
+        $this->assertEquals(1, $id);
+
+        $id = self::$table->post(['name' => 'test1']);
+
+        $this->assertEquals(2, $id);
+
+        $id = self::$table->post(['name' => 'test2']);
+
+        $this->assertEquals(3, $id);
+
+        $id = self::$table->post(['name' => 'test3']);
+
+        $this->assertEquals(4, $id);
+
+        $id = self::$table->post(['name' => null]);
+
+        $this->assertEquals(5, $id);
+
+    }
+
+
+    public function testSelectAll()
+    {
+        $rows = self::$table->get();
+
+        $this->assertEquals(5, count($rows));
+        $this->assertEquals('test', $rows[0]['name']);
+    }
+
+
+
+    public function testUpdate()
+    {
+        $r = self::$table->put(['name' => 'test1'], ['name' => 'test4']);
+
+        $this->assertEquals(1, $r);
+    }
+
+
+    public function testSelectFetch()
+    {
+        $r = self::$table->get(['name' => 'test4']);
+
+        $this->assertEquals('test4', $r['name']);
+
+    }
+
+    public function testSelectEmptyResults()
+    {
+        $r = self::$table->get(['name' => 'test5']);
+
+        $this->assertEmpty($r);
+
+    }
+
+    public function testSelectNull()
+    {
+        $r = self::$table->get(['name' => null]);
+
+        $this->assertEquals(5, $r['id']);
+    }
+
+    public function testDelete()
+    {
+        $rows = self::$table->get();
+        $this->assertEquals(count($rows), 5);
+
+        $r = self::$table->destroy(['name' => 'test1']);
+        $this->assertEquals(0, $r);
+
+        $r = self::$table->destroy(['name' => 'test4']);
+        $this->assertEquals(1, $r);
+
+        $rows = self::$table->get();
+        $this->assertEquals(count($rows), 4);
+
+    }
+
+
+}
